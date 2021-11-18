6e7a186a13305c6b871f70deac67d0a98950d5bf
Tomas Kindahl <tomas.kindahl@molk.com>
2021-11-18 07:45:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace Complexity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter writer = new(@"C:\users\nisse\test.html"))
+            {
+                GenHTML.Head(writer, "Generated diagrams");
+
+                GenHTML.Write(writer, "<h1>Generated diagram 1</h1>");
+
+                GenHTML.SVGhead(writer);
+                for (int i = 0; i < 10; i++)
+                {
+                    GenHTML.RotTextAt($"{i*10000}", 30*i+27, 363, writer);
+                }
+                GenHTML.RotTextAt("100000", 327, 361, writer);
+
+                // Here draw your statistics:
+                GenHTML.Scale scale = new GenHTML.Scale { X = 1.0, Y = 1.0, xoff = 30, yoff = 40 };
+                GenHTML.PointAt(writer, scale, "red", 0.0, 0.0);
+                GenHTML.PointAt(writer, scale, "red", 10.0, 10.0);
+
+                GenHTML.SVGfoot(writer);
+
+                GenHTML.Write(writer, "<h1>Generated diagram 2</h1>");
+
+                GenHTML.SVGhead(writer);
+                for (int i = 0; i < 10; i++)
+                {
+                    GenHTML.RotTextAt($"{i * 10000}", 30 * i + 27, 363, writer);
+                }
+                GenHTML.RotTextAt("100000", 327, 361, writer);
+
+                // Here draw your statistics:
+                GenHTML.Scale scale2 = new GenHTML.Scale { X = 1.0, Y = 1.0, xoff = 30, yoff = 40 };
+                GenHTML.PointAt(writer, scale2, "blue", 0.0, 0.0);
+                GenHTML.PointAt(writer, scale2, "blue", 10.0, 10.0);
+
+                GenHTML.SVGfoot(writer);
+
+                GenHTML.Foot(writer);
+            }
+        }
+    }
+}
